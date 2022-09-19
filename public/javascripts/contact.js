21a3384a076cefa0ee7c9b1d2e4bc20f5a580655
MartinZazpe <martinzazpe1@gmail.com>
2022-09-20 01:22:19 +0200

    navbar fix, film n tv load fix, resume fix, body opacity blur,beining to fix mystory

--- public/javascripts/contact.js
+++ public/javascripts/contact.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
 
 
-    const hambMenuContainer = document.getElementById('hambmenu-container')
+    // const hambMenuContainer = document.getElementById('hambmenu-container')
     const hambMenu = document.getElementById('hamburguer-menu')
     const lateralNavbar = document.getElementById('lateral-navbar')
     const lateralMenuNavbar = document.getElementById('lateral-menu-navbar')
@@ -11,14 +11,14 @@ window.addEventListener('load', () => {
     const bar1 = document.querySelector('#hamburguer-menu .bar1')
     const bar2 = document.querySelector('#hamburguer-menu .bar2')
     const bar3 = document.querySelector('#hamburguer-menu .bar3')
-    const noDisplay = document.getElementsByClassName('noDisplay')
-    var headShotsContainer = document.getElementById('headshots-container')
+    // const noDisplay = document.getElementsByClassName('noDisplay')
+    // var headShotsContainer = document.getElementById('headshots-container')
     const copyright = document.getElementById('copyright')
     const contactContainer = document.getElementById('contact-container')
 
 
 
-    const name = document.querySelector('#name').onclick = () => {
+    document.querySelector('#name').onclick = () => {
         location.href = "/"
     }
 
