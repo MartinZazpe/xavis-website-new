4aa6833eb8f56963dd4694367950a8689cb7e402
MartinZazpe <martinzazpe1@gmail.com>
2022-09-19 15:20:37 +0200

    working navbar correctly-.

--- public/javascripts/navbarSelect.js
+++ public/javascripts/navbarSelect.js
@@ -5,44 +5,83 @@ window.addEventListener('load', () => {
     nameTitle = document.getElementById('name')
 
 
-    let sessionLink
-    let elementSelected
+    // let sessionLink
+    // let elementSelected
 
+
+    // Array.from(navbarTablet.children).forEach((element) => {
+
+    //     if (sessionStorage.getItem('linkSelected') == element.innerText) {
+    //         element.children[0].classList.add('isSelected')
+    //     }
+
+    //     element.onclick = (e) => {
+    //         console.log(element.children[0])
+    //         elementSelected = element
+    //         element.children[0].classList.add('isSelected')
+    //         sessionLink = sessionStorage.setItem('linkSelected', element.innerText)
+
+    //         Array.from(navbarTablet.children).forEach((element) => {
+    //             if (element != elementSelected) {
+    //                 element.children[0].classList.remove('isSelected')
+    //             }
+    //         })
+    //     }
+    // })
+
+
+    let currentView = window.location.pathname
+
+    console.log(currentView)
+
+    let selectedElement
 
     Array.from(navbarTablet.children).forEach((element) => {
 
-        if (sessionStorage.getItem('linkSelected') == element.innerText) {
-            element.children[0].classList.add('isSelected')
+        selectedElement = element
+
+        if (element.querySelector('li').innerText == "My headshots" && currentView == '/headshots') {
+            element.classList.add('isSelected')
         }
+        else if (element.querySelector('li').innerText == "My story" && currentView == '/myStory') {
+            element.classList.add('isSelected')
 
-        element.onclick = (e) => {
-            console.log(element.children[0])
-            elementSelected = element
-            element.children[0].classList.add('isSelected')
-            sessionLink = sessionStorage.setItem('linkSelected', element.innerText)
+        }
+        else if (element.querySelector('li').innerText == "Film & TV" && currentView == '/film&Tv') {
+            element.classList.add('isSelected')
 
-            Array.from(navbarTablet.children).forEach((element) => {
-                if (element != elementSelected) {
-                    element.children[0].classList.remove('isSelected')
-                }
-            })
+        }
+        else if (element.querySelector('li').innerText == "Resume" && currentView == '/myResume') {
+            element.classList.add('isSelected')
+
+        }
+        else if (element.querySelector('li').innerText == "contact" && currentView == '/contact') {
+            element.classList.add('isSelected')
+        }
+        else if (element.querySelector('li').innerText == `Let's talk` && currentView == '/contact') {
+            element.classList.add('isSelected')
         }
     })
 
 
-    nameTitle.onclick = async () => {
-        let result = new Promise((resolve, reject) => {
-            Array.from(navbarTablet.children).forEach((element) => {
-                element.children[0].classList.remove('isSelected')
-            })
-            resolve('foo')
-        })
-        result.then(() => {
-            sessionStorage.clear()
-            location.href = "/"
-        })
 
-    }
+
+
+
+
+    // nameTitle.onclick = async () => {
+    //     let result = new Promise((resolve, reject) => {
+    //         Array.from(navbarTablet.children).forEach((element) => {
+    //             element.children[0].classList.remove('isSelected')
+    //         })
+    //         resolve('foo')
+    //     })
+    //     result.then(() => {
+    //         sessionStorage.clear()
+    //         location.href = "/"
+    //     })
+
+    // }
 
 
 
